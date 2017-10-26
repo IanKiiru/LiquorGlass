9c719eaf3805ad9763ee9c4a451b91ae2144a9e3
Kiiru <iankiiru95@gmail.com>
2017-10-26 06:51:05 +0300

    Creating remember me checkbox

--- app/src/main/java/com/example/kiiru/liquorglass/UserProfile.java
+++ app/src/main/java/com/example/kiiru/liquorglass/UserProfile.java
@@ -1,5 +1,12 @@
 package com.example.kiiru.liquorglass;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,14 +14,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.kiiru.liquorglass.common.Common;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,7 +48,10 @@ public class UserProfile extends AppCompatActivity {
     private String email;
     private String phone;
     private String password;
+
     private String mProfileImageUrl;
+    private Uri resultUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +74,15 @@ public class UserProfile extends AppCompatActivity {
 
         getUserInfo();
 
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profileImageIntent = new Intent(Intent.ACTION_PICK);
+                profileImageIntent.setType("image/*");
+                startActivityForResult(profileImageIntent, 1);
+            }
+        });
+
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +101,10 @@ public class UserProfile extends AppCompatActivity {
     }
 
     private void getUserInfo(){
+        final ProgressDialog mProgressDialog = new ProgressDialog(UserProfile.this);
+        mProgressDialog.setTitle("Fetching your details...");
+        mProgressDialog.setMessage("Please wait...");
+        mProgressDialog.show();
         mCustomerDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -99,6 +130,10 @@ public class UserProfile extends AppCompatActivity {
                         password = map.get("password").toString();
                         passwordField.setText(password);
                     }
+                    if(map.get("profileImageUrl")!=null){
+                        mProfileImageUrl = map.get("profileImageUrl").toString();
+                        Glide.with(getApplication()).load(mProfileImageUrl).into(profileImageView);
+                    }
                 }
 
             }
@@ -108,10 +143,15 @@ public class UserProfile extends AppCompatActivity {
 
             }
         });
+        mProgressDialog.dismiss();
 
     }
 
     private void saveUserInformation() {
+        final ProgressDialog mProgressDialog = new ProgressDialog(UserProfile.this);
+        mProgressDialog.setMessage("Saving your details...");
+        mProgressDialog.show();
+
         email = emailField.getText().toString();
         firstName = firstNameField.getText().toString();
         lastName = lastNameField.getText().toString();
@@ -125,6 +165,55 @@ public class UserProfile extends AppCompatActivity {
         userInfo.put("phone", phone);
         userInfo.put("password", password);
         mCustomerDatabase.updateChildren(userInfo);
-        finish();
+
+        if(resultUri != null) {
+
+            StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    mProgressDialog.dismiss();
+                    finish();
+                    return;
+                }
+            });
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    Map newImage = new HashMap();
+                    newImage.put("profileImageUrl", downloadUrl.toString());
+                    mCustomerDatabase.updateChildren(newImage);
+
+                    finish();
+                    return;
+                }
+            });
+        }else{
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            profileImageView.setImageURI(resultUri);
+        }
     }
 }
